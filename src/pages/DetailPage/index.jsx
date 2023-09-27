7db302a56c7a4423cb030e9cefd44175e79516cf
sshinn0224 <sshinn0224@gmail.com>
2023-09-27 11:09:08 +0900

    detail page

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  const params = useParams()
+  const pokemonId = params.id
+  console.log(params)
+
+  useEffect(() => {
+    fetchPokemonData()
+  }, [])
+
+  async function fetchPokemonData() {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+
+    try {
+      const { data } = await axios.get(url)
+      console.log(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return <div>DetailPage</div>
 }
 
